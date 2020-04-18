3061ea8fe804da114a3a4dc69089794a45492e8c
Nitin Kumar <work.nitinkumar@gmail.com>
2020-04-19 02:40:41 +0530

    added get all methods for consignments

--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -16,6 +16,7 @@ const port = ":50051"
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 type Repository struct {
@@ -31,6 +32,10 @@ func (repo *Repository) Create(consignment *pb.Consignment)  (*pb.Consignment, e
 	return consignment, nil
 }
 
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
 type service struct {
 	repo repository
 }
@@ -43,6 +48,10 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	return &pb.Response{Created: true,Consignment: consignment}, nil
 }
 
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
+}
 
 func main() {
 	repo := &Repository{}
