d83ab55a2da74b63a3e380bba6981b779f67c911
Nitin Kumar <work.nitinkumar@gmail.com>
2020-04-19 02:26:03 +0530

    added cli for creating consignment

---
+++ shippy-service-consignment/shippy-cli-consignment/main.go
@@ -0,0 +1,29 @@
+package main
+
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"context"
+
+	pb "shippy-service-consignment/proto/consignment"
+	"google.golang.org/grpc"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
